e8653ae47de9de090000b381eb6f7604dcfe5306
sohag <shcseilacs@gmail.com>
2022-03-01 11:16:11 +0600

    Added UserFactory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,11 +8,14 @@ class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
-     *
+     * lk,.
      * @return void
      */
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call([
+            UserSeeder::class,
+        ]);
     }
 }
